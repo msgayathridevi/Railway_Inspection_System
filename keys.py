a57361c821ba6ccc1d23b50057d99b0dd8f3aae3
msgayathridevi <59758501+msgayathridevi@users.noreply.github.com>
2023-02-18 15:44:10 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -2,4 +2,4 @@ account_sid = 'AC1cffad1aa889fe2c6e47051b7000bedb'
 auth_token = '74d4dc86d71fb82ee010569df3978439'
 
 twilio_number = '+18654326911'
-my_phone_number = '+919976280070'
+my_phone_number = '(designation phone number with country code)'
