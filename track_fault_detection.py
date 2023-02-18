d1b0a19b03931fdf0386eec05034bffefae7731e
msgayathridevi <59758501+msgayathridevi@users.noreply.github.com>
2023-02-18 15:46:47 +0530

    Update track_fault_detection.py

--- track_fault_detection.py
+++ track_fault_detection.py
@@ -1,4 +1,4 @@
-# Download the helper library from https://www.twilio.com/docs/python/install
+
 import os
 from twilio.rest import Client
 import keys
@@ -11,21 +11,3 @@ message = client.messages.create(
 )
 
 print(message.body)
-
-    
-"""
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
-client = Client(account_sid, auth_token)
-
-message = client.messages \
-                .create(
-                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_='+919976280070',
-                     to='+918300843618'
-                 )
-
-print(message.sid)
-"""
